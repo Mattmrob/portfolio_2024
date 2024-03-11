52bac9f01ac5dd113bde3f8d33124c5af266e481
Mysmelf <matthewr9937@gmail.com>
2024-03-11 11:31:22 -0700

    Created sliding door animation for intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,13 @@ export default function Home() {
         <div className="landingContainer">
           <img className="mainBackground" src="assets/imgs/background.jpg"></img>
           <img className="logo" src="assets/imgs/mmrobertslogo.svg"></img>
+          <div className="spinnerRing"></div>
+          <div className="spinner1"></div>
+          <div className="spinner2"></div>
+          <div className="door1"></div>
+          <div className="door2"></div>
         </div>
+        <h1> test </h1>
       </div>
     );
   }
